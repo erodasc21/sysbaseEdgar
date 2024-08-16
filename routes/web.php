c50d6380022dceaa52af693c27d0a574b9696aaf
Edgar Rodas <erodascarias@gmail.com>
2024-08-16 14:50:31 -0600

    Revert "Creación de modelo y migraciones"
    This reverts commit 77985162a0483472a1802875e47cb2b24608cc84.

--- routes/web.php
+++ routes/web.php
@@ -68,11 +68,9 @@ Route::group(['prefix' => 'admin','middleware' => ['role:Admin|Superadmin|Develo
     Route::resource('permissions', PermissionController::class);
 
 
-    Route::resource('capacitacionEstados', App\Http\Controllers\CapacitacionEstadosController::class);
-    Route::resource('capacitacionTipos', App\Http\Controllers\CapacitacionTipoController::class);
-    Route::resource('capacitacionClientes', App\Http\Controllers\CapacitacionClienteController::class);
 
 });
+
 
 
 
@@ -97,10 +95,5 @@ Route::group(['prefix' => ''], function () {
         ->name('cambio.idioma');
 
 
-
 });
 
-
-
-Route::resource('capacitacionModelos', App\Http\Controllers\CapacitacionModeloController::class);
-Route::resource('capacitacionMarcas', App\Http\Controllers\CapacitacionMarcaController::class);
